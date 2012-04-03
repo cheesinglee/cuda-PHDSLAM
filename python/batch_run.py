d54253f5c5525da110207e888b2664833c92e080
chee sing <cheesinglee@gmail.com>
2012-04-03 17:23:49 +0100

    Modified batch run script for batch inputs located in /data dir
    Signed-off-by: chee sing <cheesinglee@gmail.com>

--- python/batch_run.py
+++ python/batch_run.py
@@ -6,23 +6,31 @@ Created on Tue Sep  6 18:51:27 2011
 """
 
 import os
+import shutil
 import re
 import subprocess
 
-n_runs = 50
+n_runs = 100
 project_dir = '/home/cheesinglee/workspace/cuda-PHDSLAM'
+data_dir = project_dir + os.path.sep + 'data/synth_mixed'
 measurements_regex = r'(measurements_filename\s*=\s*).+'
 controls_regex = r'(controls_filename\s*=\s*).+'
 
-for n in xrange(n_runs):
+for n in xrange(1,n_runs+1):
     print 'run #: ',n    
     
     # set the working directory 
-    os.chdir(project_dir+'/python/'+str(n))
+    os.chdir(data_dir+os.path.sep+str(n))
+    
+    # remove previous results
+    contents = os.listdir(os.getcwd())
+    for f in contents:
+        if os.path.isdir(f):
+            shutil.rmtree(f)
     
     # modify the config file
-    measurements_path = project_dir+'/python/'+str(n)+'/measurements_synth.txt'
-    controls_path = project_dir+'/python/'+str(n)+'/controls_synth.txt'
+    measurements_path = data_dir+os.path.sep+str(n)+'/measurements_synth.txt'
+    controls_path = data_dir+os.path.sep+str(n)+'/controls_synth.txt'
     config_file = open(project_dir+'/cfg/config.cfg','rw+')
     config_str = config_file.read() 
     config_str = re.sub(measurements_regex,r'\1'+measurements_path,config_str)
